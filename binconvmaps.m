5f78540beace13830049ac8f9297def2693b457e
Giulio <giulioperetti@gmail.com>
2013-04-29 18:47:32 +0200

    binconvmaps updated, but not yet checked

--- binconvmaps.m
+++ binconvmaps.m
@@ -1,17 +1,21 @@
-function [S,O,N] = binconvmaps(n)
-%BINCONVENC Build maps of a binary convolution encoder from the generator polynomials in binary vector form.
-%[S,O,N] = binconvec(n) 
+function [S,O,N] = binconvmaps(n1,n2)
+%BINCONVENC Build maps of a rate 1/2 binary convolution encoder from the 
+%generator polynomials in binary vector form.
+%[S,O,N] = binconvec(n1,n2) 
 % S(s,u) is the state update function matrix
 % O(s,u) is the output function matrix
 % N(s) is the neghbour vector
     
-    nu = length(n)-1; % memory of the system
+    
+    nu = length(n1)-1; % memory of the system
     %s = gf(zeros(1,nu)); % state of the finite state machine
     
     O = zeros(2^nu,2); % initialization of the output map
     for i=1:2^nu
-        O(i,1) = n*[0 de2bi(i-1,nu,'left-msb')].';
-        O(i,2) = n*[1 de2bi(i-1,nu,'left-msb')].';
+        O(i,1) = bi2de(mod([n1*[0 de2bi(i-1,nu,'left-msb')].' 
+            n2*[0 de2bi(i-1,nu,'left-msb')].'].',2),'left-msb');
+        O(i,2) = bi2de(mod([n1*[1 de2bi(i-1,nu,'left-msb')].' 
+            n2*[0 de2bi(i-1,nu,'left-msb')].'].',2),'left-msb');
     end
     O = mod(O,2);
     
