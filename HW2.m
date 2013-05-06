f32f1bb2cc07c7833ea0e195651f9341598c0059
Giulio <giulioperetti@gmail.com>
2013-05-06 11:31:07 +0200

    Plot implementation

--- HW2.m
+++ HW2.m
@@ -14,9 +14,8 @@ g1 = [1 0 1];
 g2 = [1 1 1];
 
 % input
-mu = 10^6; 
+mu = 10^7; 
 u = round(rand(1,mu));
-%u = [1 0 1];
 
 % maps
 [SS,OO,NN] = binconvmaps(g1,g2);
@@ -26,10 +25,25 @@ y = cbinconvenc(u,SS,OO);
 %y = binconvenc(u,SS,OO);
 
 % modulation
-r = pamap(y);
+s = pamap(y);
 
-% viterbi decoding
-uhat = cbinconvdec(r,SS,OO,NN);
+% channel simulation
+Eb_N0_max = 8;
+Eb_N0_dB = 0:Eb_N0_max;
+snr_dB = Eb_N0_dB - 10*log10(0.5);
+errors = zeros(1,Eb_N0_max);
 
-errors = sum(u-uhat)
+for i=1:Eb_N0_max+1
 
+    r = awgn(s,snr_dB(i),'measured');
+
+    % viterbi decoding
+    uhat = cbinconvdec(r,SS,OO,NN);
+    %uhat = binconvdec(r,SS,OO,NN);
+
+    errors(i) = sum(abs(u-uhat))/mu;
+    
+end
+
+semilogy(Eb_N0_dB,errors);
+grid on;
